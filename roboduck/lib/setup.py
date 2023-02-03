b2f6c67b8d9cb22ef97064b71c277ba1df243d1f
hdmamin <hmamin2@dons.usfca.edu>
2023-02-02 19:52:40 -0800

    roboduck subdir

---
+++ roboduck/lib/setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+
+def requirements(path='requirements.txt'):
+    with open(path, 'r') as f:
+        deps = [line.strip() for line in f]
+    return deps
+
+
+def version():
+    path = os.path.join('roboduck', '__init__.py')
+    with open(path, 'r') as f:
+        for row in f:
+            if not row.startswith('__version__'):
+                continue
+            return row.split(' = ')[-1].strip('\n').strip("'")
+
+
+setuptools.setup(name='roboduck',
+                 version=version(),
+                 author='Harrison Mamin',
+                 author_email='harrisonmamin@gmail.com',
+                 description='A natural language debugger.',
+                 install_requires=requirements(),
+                 packages=setuptools.find_packages())
+
