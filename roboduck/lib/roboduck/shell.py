4fa2a876a767978726604a99887697fbcd8eafd8
hdmamin <hmamin2@dons.usfca.edu>
2023-02-25 15:42:00 -0800

    add y/n prompt, pass in stack trace to excepthook

--- roboduck/lib/roboduck/shell.py
+++ roboduck/lib/roboduck/shell.py
@@ -3,6 +3,10 @@ from pdb import Pdb
 
 
 class RoboDuckTerminalInteractiveShell(TerminalInteractiveShell):
-    """TODO: docs"""
+    """Allows roboduck magic to work in ipython. Ipython uses
+    TerminalInteractiveShell which makes debugger_cls attribute read only.
+    We overwrite it here so that our magic class can set it to our desired
+    class when requested.
+    """
 
     debugger_cls = Pdb
