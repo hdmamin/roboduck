918420025524955e26255b9a94e548ee25527fde
hdmamin <hmamin2@dons.usfca.edu>
2023-05-16 22:52:05 -0700

    better error handling in cli; cli docs

--- docs/cli/cli.md
+++ docs/cli/cli.md
@@ -1,3 +1,1 @@
-# TODO: not working. Also tried leading ".." but that throws empty import error.
-# Tried both lib.roboduck.cli and lib.roboduck.cli.cli but both failed.
-::: lib.roboduck.errors
+::: lib.roboduck.cli.cli
--- lib/roboduck/cli/cli.py
+++ lib/roboduck/cli/cli.py
@@ -19,11 +19,13 @@ def make_import_statement(cls_name):
     ----------
     cls_name : str
         Class name including module (essentially __qualname__?), e.g.
-        roboduck.debug.DuckDB.
+        roboduck.DuckDB. (Note that this would need to be roboduck.debug.DuckDB
+        if we didn't include DuckDB in roboduck's __init__.py.)
 
     Returns
     -------
     str
+        E.g. "from roboduck import DuckDB"
     """
     parts = cls_name.split('.')
     if len(parts) == 1:
@@ -34,15 +36,24 @@ def make_import_statement(cls_name):
 
 
 def run():
-    """Execute a python script with auto error mode enabled."""
+    """Execute a python script with auto error mode enabled.
+
+    Run a python script with roboduck\'s errors mode
+    automatically enabled.
+
+    Examples
+    --------
+    Make sure to include the equals sign between option name and value.
+    If using a custom chat_class, the full name must be provided.
+
+    ```
+    duck my_script.py
+    duck my_script.py --chat_class=roboduck.DummyChatClass
+    duck my_script.py --auto=True --prompt_name=~/my_custom_prompt.yaml
+    ```
+    """
     parser = argparse.ArgumentParser(
-        description='Run a python script with roboduck\'s errors mode '
-                    'automatically enabled.\n\nExamples:\nMake sure to include'
-                    'the equals sign between option name and value.'
-                    '\n\nduck my_script.py'
-                    '\nduck my_script.py --chat_class=roboduck.DummyChatClass'
-                    '\nduck my_script.py --auto=True '
-                    '--prompt_name=~/my_custom_prompt.yaml',
+        description=run.__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument('file', help='The python script to execute.')
@@ -67,7 +78,6 @@ def run():
     tmp_path = Path('/tmp')/path.name
     with open(path, 'r') as f:
         src_text = f.read()
-    path.rename(tmp_path)
     new_text = 'from roboduck import errors\n' + '\n'.join(imports) + '\n'
     if kwargs:
         kwargs_str = ''
@@ -79,18 +89,29 @@ def run():
                 kwargs_str += repr(v)
             kwargs_str += ', '
         new_text += f'errors.enable(**dict({kwargs_str}))\n'
-    src_text = new_text + src_text
+    modified_text = new_text + src_text
 
     # Create copy file with imports and errors enabled, try to execute it, then
-    # restore original file.
+    # restore original file. Keep all file renaming and writing inside try
+    # block to avoid confusion caused by premature sigints.
     try:
+        path.rename(tmp_path)
         with open(path, 'w') as f:
-            f.write(src_text)
+            f.write(modified_text)
         subprocess.call(['python', str(path)])
     except Exception as e:
         raise e
+    # At one point I observed an error where a sigint during error explanation
+    # resulted in tmp_path not being found and the file at the user-specified
+    # path was left with the roboduck errors import. I believe moving the
+    # `path.rename` step inside the try block fixed that but to be safe,
+    # we add some additional error handling.
     finally:
-        tmp_path.rename(path)
+        if tmp_path.is_file():
+            tmp_path.rename(path)
+        else:
+            with open(path, 'w') as f:
+                f.write(src_text)
 
 
 if __name__ == '__main__':
