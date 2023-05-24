1a52d309296f60333deb73e605c7f3dc68b47694
hdmamin <hmamin2@dons.usfca.edu>
2023-05-23 21:02:06 -0700

    0.3.0

--- lib/roboduck/__init__.py
+++ lib/roboduck/__init__.py
@@ -4,5 +4,5 @@ from roboduck.config import update_config, load_config, set_openai_api_key
 from roboduck.utils import available_models
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 set_openai_api_key()
