1e45a2c2f97488bf9280e406f09248c5d64759cd
hdmamin <hmamin2@dons.usfca.edu>
2023-05-25 22:14:45 -0700

    add debug example to quickstart nb

--- lib/roboduck/debug.py
+++ lib/roboduck/debug.py
@@ -13,7 +13,7 @@ from roboduck import duck
 
 def bubble_sort(nums):
     for i in range(len(nums)):
-        for j in range(len(nums)):
+        for j in range(len(nums) - 1):
             if nums[j] > nums[j + 1]:
                 nums[j + 1] = nums[j]
                 nums[j] = nums[j + 1]
