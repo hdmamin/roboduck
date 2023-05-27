4d2af02a88a0467a8a51fdf9f8396ae0be30cae7
hdmamin <hmamin2@dons.usfca.edu>
2023-05-26 21:20:07 -0700

    try removing div from lib readme

--- lib/roboduck/__init__.py
+++ lib/roboduck/__init__.py
@@ -7,7 +7,7 @@ from roboduck.ipy_utils import is_colab
 from roboduck.utils import available_models
 
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 set_openai_api_key()
 if is_colab():
     warnings.warn(
