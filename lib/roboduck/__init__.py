ec168aefffb803473c85b2dce342c5ba6573b961
hdmamin <hmamin2@dons.usfca.edu>
2023-05-26 21:15:46 -0700

    0.3.1

--- lib/roboduck/__init__.py
+++ lib/roboduck/__init__.py
@@ -7,7 +7,7 @@ from roboduck.ipy_utils import is_colab
 from roboduck.utils import available_models
 
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 set_openai_api_key()
 if is_colab():
     warnings.warn(
