06cc63ff96f23f30410b6e7974a6dd37a25042cb
hdmamin <hmamin2@dons.usfca.edu>
2024-09-01 23:23:16 -0700

    allow custom config path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,14 @@
 import os
 import pytest
 
-from roboduck import set_openai_api_key
+from roboduck.config import set_openai_api_key, CONFIG_PATH_ENV_VAR
 
 
 @pytest.fixture(autouse=True)
 def set_dummy_openai_api_key(tmp_path):
     file_path = tmp_path/'config.yaml'
-    set_openai_api_key('xyz', config_path=file_path, update_config_=True)
-    os.environ['CONFIG_FILE'] = str(file_path)
+    os.environ[CONFIG_PATH_ENV_VAR] = str(file_path)
+    set_openai_api_key('xyz', update_config_=True)
     # File will exist while all tests are running, then get deleted afterwards.
     yield
 
