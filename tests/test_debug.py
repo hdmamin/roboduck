5d2fee1cf6aad83a5c32dc36bb1c914d5d39e155
hdmamin <hmamin2@dons.usfca.edu>
2024-09-19 22:41:56 -0700

    test field names

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -26,3 +26,23 @@ from roboduck.debug import DuckDB
 def test_is_conversational_reply(line: str, expected: bool):
     debugger = DuckDB()
     assert debugger._is_conversational_reply(line) == expected
+
+
+@pytest.mark.parametrize(
+        'key,expected_names',
+        (
+            (None,
+             {'code', 'global_vars', 'local_vars', 'next_line', 'question'}),
+            ('contextful',
+             {'code', 'global_vars', 'local_vars', 'next_line', 'question'}),
+            ('contextless',
+             {'question'}),
+        )
+)
+def test_field_names(key: str, expected_names: 'set[str]'):
+    debugger = DuckDB()
+    kwargs = {}
+    if key is not None:
+        kwargs['key'] = key
+    field_names = debugger.field_names(**kwargs)
+    assert field_names == expected_names
